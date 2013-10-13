fcb6bd1230f83561d03333311c31634ac75254e1
daniel@numlock.se <daniel@numlock.se>
2013-10-13 21:06:51 +0200

    init

---
+++ HomeController.TelldusIntegration/HookUpTelldus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HomeController.TelldusIntegration.Dtos;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace HomeController.TelldusIntegration
+{
+    public class HookUpTelldus
+    {
+        private const string TelldusBaseUrl = "https://api.telldus.com/json";
+        private string _publickey = "FEHUVEW84RAFR5SP22RABURUPHAFRUNU";
+        private string _privateKey = "ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF";
+        private string _token = "408b287b4c4cca887c20f4bf6179a3b70525acb9f";
+        private string _tokenSecret = "b6f0efcf16e880a962dcb16ccf5b06f5";
+
+        public void Hookitup()
+        {
+            var client = new RestClient(TelldusBaseUrl);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(_publickey, _privateKey, _token, _tokenSecret);
+            var request = new RestRequest("devices/list");
+            var response = client.Execute(request);
+            //client.ExecuteAsync(request, response =>
+            //{
+            //    TakeResponse();
+            //});
+        }
+
+        public List<Sensor> GetSensors()
+        {
+            var client = new RestClient(TelldusBaseUrl);
+            client.Authenticator = OAuth1Authenticator.ForProtectedResource(_publickey, _privateKey, _token, _tokenSecret);
+            var request = new RestRequest("sensors/list");
+            var response = client.Execute<RootObject>(request);
+            return response.Data.sensor;
+            
+        }
+    }
+}
---
+++ Tests/HomeController.IntegrationTests/TelldusTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HomeController.TelldusIntegration;
+using NUnit.Framework;
+
+namespace HomeController.IntegrationTests
+{
+    [TestFixture]
+    public class TelldusTests
+    {
+        [Test]
+        public void hookTeldus()
+        {
+            var teller = new HookUpTelldus();
+            teller.Hookitup();
+
+        }
+        [Test]
+        public void GetSensors()
+        {
+            var teller = new HookUpTelldus();
+            var sensors = teller.GetSensors();
+            Assert.That(sensors, Is.Not.Null);
+            Assert.That(sensors.Count, Is.EqualTo(2));
+            foreach (var sensor in sensors)
+            {
+                Console.WriteLine(sensor.name + " " + sensor.LastUpdated);
+            }
+
+        }
+
+    }
+}
